fc51b8a81e56d5301d0bc897144b177ada17bca9
maximilian.zangerl@student.htl-rankweil.at <maximilian.zangerl@student.htl-rankweil.at>
2024-11-09 16:37:52 +0000

    Edited Startseite

--- client_code/Startseite/__init__.py
+++ client_code/Startseite/__init__.py
@@ -8,14 +8,30 @@ class Startseite(StartseiteTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    self.rooms = [
+    (zimmernummer, bettenanzahl, preis_pro_nacht, gebucht, JID, ZID)
+    for zimmernummer, bettenanzahl, preis_pro_nacht, gebucht, JID, ZID in anvil.server.call("get_rooms")
+    ]
     # Any code you write here will run before the form opens.
     self.drop_down_1.items = [("Feldkirch", 0), ("Hohenems", 1)]
     print(anvil.server.call("say_hello", "42"))
     self.drop_down_1.items = anvil.server.call("get_jugendherbergen", "name, JID")
 # If needed, transform the items for display or store multiple values in a way that's compatible
-    self.dropdownzimmer.items = [(f"Zimmer {zimmernummer} - {bettenanzahl} Betten  - Preis pro Nacht: {preis_pro_nacht} - Verfügbarkeit: {gebucht}", ZID) for zimmernummer, bettenanzahl, preis_pro_nacht, gebucht, JID, ZID in anvil.server.call("get_rooms")]
 
 # Fügt die Daten zur Data Table hinzu
+   
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    matching_rooms = []
+    selected_value = self.drop_down_1.selected_value
+    for i in self.rooms:
+      print(self.rooms)
+      if i[4] == selected_value:
+        print(i[4])
+        matching_rooms.append(f"Zimmernummer:{i[0]}  -  Bettenanzahl: {i[1]}  -  Preis p. Nacht = {i[2]}  -  Gebucht: {i[4]}")
+    self.dropdownzimmer.items = matching_rooms
+    
+    print("Selected Index:", selected_value)
+    pass
     
 
